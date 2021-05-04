388d2b460d4c9f01d109ad60755fa921302b5eaf
arya jayan <arya.kjayan@gmail.com>
2021-05-04 17:26:19 +0530

    day2 exersise

---
+++ day2/q2.py
@@ -0,0 +1,15 @@
+# Write a name creator app
+
+# Ask user's first name
+# Ask user's last name
+# Print like Firstname Lastname (capialized first and last names)
+
+fname=input("Enter First Name : ")
+lname=input("Enter Last Name : ")
+print(fname.upper() + " " + lname.upper())
+
+
+# --Output--
+# Enter First Name : Arya 
+# Enter Last Name : Jayan
+# ARYA JAYAN
---
+++ day2/q3.py
@@ -0,0 +1,17 @@
+# Accept 2 lists of numbers from user and
+
+# print numbers present in both the lists
+# print numbers in one list and not another and vice versa
+
+list1={2,3,5,3,7}
+list2={8,9,0,3,2}
+print(list1 & list2)
+print(list1 - list2)
+print(list2 - list1)
+
+
+
+# --Output--
+# {2, 3}
+# {5, 7}
+# {0, 8, 9}
