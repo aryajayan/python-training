b91130c1cff1f0c620a318f29ce3754cd40c3be4
arya jayan <arya.kjayan@gmail.com>
2021-05-07 12:03:03 +0530

    day4- correction

--- day4/q2.py
+++ day4/q2.py
@@ -3,11 +3,13 @@
 # a bit of custom logic to figure out the stop value.In case of get_multiples(3), 
 # output should be 3 6 9 12 15. N can be any positive int. not just 3 or 5.
 
+def get_multiples(n):
+    for i in range(n,n*6, n):
+        print(i, end=" ")
+    print()
+
 num=int(input("Enter the number : "))
-
-for i in range(1,6):
-    print(num*i, end=" ")
-
+get_multiples(num)
 # --output--
 # Enter the number : 5
 # 5 10 15 20 25 
